4baeec1e4006dcf138fda0775075e67990b87501
== <==>
2024-01-03 11:46:56 +0000

    pagination calculation helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,7 +2,18 @@
 """Helper function"""
 
 def index_range(page, page_size):
-    """Funciton"""
-    start_idx = page
-    end_idx = range(start_idx, page_size)
-    return [end_idx, start_idx]
+    """
+        Calculate the start and end index for the items on a specific page.
+
+        Arguments:
+        page -- The current page number (1-indexed).
+        page_size -- The number of items on each page.
+
+        Returns:
+        A tuple containing the start and end index for the items on the specified page.
+    """
+    # Calculating start index
+    start_idx = (page - 1) * page_size
+    # Calculating end index
+    end_idx = page * page_size
+    return (end_idx, start_idx)
