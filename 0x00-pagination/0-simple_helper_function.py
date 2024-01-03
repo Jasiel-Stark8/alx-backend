9013b73db54e28d5710fe6d7386d9b08fb90a9c0
== <==>
2024-01-03 11:24:17 +0000

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Helper function"""
+
+def index_range(page, page_size):
+    """Funciton"""
+    start_idx = page
+    end_idx = range(start_idx, page_size)
+    return [end_idx, start_idx]
