d432207434c110d66179b597c6c571b076393962
== <==>
2024-01-04 01:11:32 +0000

    FIFO Caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
-"""_summary_"""
-from typing import Union
+""" FIFOCache module. """
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """
-    Class
-    """
-    def __init__(self):
-        """Sumary_line"""
-        super().__init__()
-        self.key_order = []
+    """ FIFOCache class that inherits from BaseCaching.
+    Implements the FIFO caching algorithm. """
 
-    def put(self, key: Union[str, int], item: Union[str, int]) -> None:
-        """sumary_line"""
+    def __init__(self):
+        """ Initialize the class. """
+        super().__init__()
+        self.key_order = []  # List to keep track of the order keys were added
+
+    def put(self, key, item):
+        """ Assign the item value for the key in self.cache_data.
+        If the cache exceeds its limit, remove the first item added. """
         if key is not None and item is not None:
             if key not in self.cache_data:
-                self.key_order.append(key)
+                self.key_order.append(key)  # Track key order
 
-            self.cache_data[key] = item
+            self.cache_data[key] = item  # Add or update the key-value pair
 
-            if len(self.cache_data[key]) > BaseCaching.MAX_ITEMS:
-                discarded_key = self.key_order.pop(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # Remove the first item added (FIFO)
+                discarded_key = self.key_order.pop(0)
                 del self.cache_data[discarded_key]
                 print(f"DISCARD: {discarded_key}")
-        return None
 
     def get(self, key):
-        """sumary_line"""
+        """ Return the value linked to the key from self.cache_data.
+        If key is None or doesn’t exist, return None. """
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
