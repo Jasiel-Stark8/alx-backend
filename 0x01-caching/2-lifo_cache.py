80f23e794de5300b9fa8eb7d11c2846dc10ce354
== <==>
2024-01-04 01:43:41 +0000

    LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,18 +5,18 @@ from base_caching import BaseCaching
 
 class LIFOCache(BaseCaching):
     """LIFOCache Class implements Last-In-First-Out caching strategy"""
-
     def __init__(self):
-        """Initialize"""
+        """Initialize Class"""
         super().__init__()
         self.key_order = []
 
     def put(self, key, item):
-        """Assign the item value for the key in self.cache_data.
-        If the cache exceeds its limit, remove the last item added."""
+        """
+        Assign the item value for the key in self.cache_data.
+        If the cache exceeds its limit, remove the last item added
+        """
         if key is not None and item is not None:
             if key in self.cache_data:
-
                 self.key_order.remove(key)
             else:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
@@ -28,8 +28,10 @@ class LIFOCache(BaseCaching):
             self.key_order.append(key)
 
     def get(self, key):
-        """Return the value linked to the key from self.cache_data.
-        If key is None or doesn’t exist, return None."""
+        """
+        Return the value linked to the key from self.cache_data.
+        If key is None or doesn't exist, return None.
+        """
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
