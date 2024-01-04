792993d5a76b1ad7143f9cdd0a55214c45df2af7
== <==>
2024-01-04 03:11:21 +0000

    LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,3 +1,35 @@
 #!/usr/bin/python3
-"""_summary_"""
+"""LRUCache Module"""
 from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache class that inherits from BaseCaching.
+    Implements the LRU caching algorithm."""
+
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.key_order = []
+
+    def put(self, key, item):
+        """sumary_line"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.key_order.remove(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = self.key_order.pop(0)
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+
+        self.cache_data[key] = item
+        self.key_order.append(key)
+
+    def get(self, key):
+        """Get cache"""
+        if key is not None and key in self.cache_data:
+            self.key_order.remove(key)
+            self.key_order.append(key)
+            return self.cache_data[key]
+        return None
