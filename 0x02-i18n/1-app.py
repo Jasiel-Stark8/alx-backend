ca13c4b22561a69e8d87b56d9cdbcd9b849374b8
jason <nftsingularity8@gmail.com>
2024-01-18 13:35:06 +0000

    Flask Babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,19 +7,18 @@ from flask_babel import Babel
 app = Flask(__name__)
 app.config['BABEL_DEFAULT_LOCALE'] = ["en", "fr"]
 app.config['BABEL_DEFAULT_TIMEZONE'] = ['UTC']
-babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale():
     """Return default language as en"""
     return request.accept_languages.best_match(['en', 'fr'])
 
 
-@babel.timezoneselector
 def get_timezone():
     """Get Timezone else default to UTC"""
     return request.timezone
+
+babel = Babel(app, locale_selector=get_locale, timezone_selector=get_timezone)
 
 
 @app.route('/')
