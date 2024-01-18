9a67c3092952822a763a9dd2d7d0964b03b76583
jason <nftsingularity8@gmail.com>
2024-01-18 19:21:47 +0000

    Flask Mock Login

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -3,8 +3,6 @@
 
 from flask import Flask, render_template, g, request
 from flask_babel import Babel
-from typing import Union, Dict, Integer, String
-from functools import wrapper
 
 
 class Config(object):
@@ -22,15 +20,12 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """Get locale"""
-    # Get locale from URL parameter
     url_locale = request.args.get('locale')
     if url_locale and url_locale in Config.LANGUAGES:
         return url_locale
-
-    # Check User-specific locale preference
     user = getattr(g, 'user', None)
-    if user is not None and user.locale in Config.LANGUAGES:
-        return user.locale
+    if user is not None and user.get('locale') in Config.LANGUAGES:
+        return user.get('locale')
     return request.accept_languages.best_match(Config.LANGUAGES)
 
 
@@ -38,17 +33,14 @@ def get_locale():
 @app.before_request
 def before_request():
     """"""
-    user_id = request.args.get('login_as')
-    if user_id:
-        g.user = get_user(users, int(user_id))
-    else:
-        g.user = None
+    user_id = login_as()
+    g.user = get_user(users, int(user_id)) if user_id else None
         
 
 @app.route('/')
 def home():
     """Return home page"""
-    return render_template('3-index.html')
+    return render_template('5-index.html')
 
 
 users = {
@@ -58,21 +50,15 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-def login_as() -> Union[Integer, String]:
+def login_as():
     """Login as a user in mock schema"""
     user_id = request.args.get('id')
-    if user_id in users[id]:
-        return user_id
-    return 'User not found'
+    return int(user_id) if user_id and user_id.isdigit() and int(user_id) in users else None
 
-def get_user(users, user_id: int) -> Union[Dict, None]:
+
+def get_user(users, user_id: int):
     """User mock login"""
-    valid_login = login_as(user_id)
-    if valid_login:
-        if user_id is not None:
-            return users[user_id]
-    return None
-
+    return users.get(user_id)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
