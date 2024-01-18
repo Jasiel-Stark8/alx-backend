133ace588d0255f8040c06916f01fe97aebbdd7c
jason <nftsingularity8@gmail.com>
2024-01-18 17:43:52 +0000

    Flask Babel_

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -5,15 +5,22 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
+class Config(object):
+    """Babel Config Class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config.from_object(Config)
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
     """Get locale"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/')
