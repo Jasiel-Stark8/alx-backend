a22d2b824b23b1e3fc6dfa31fadda8b35168e77b
jason <nftsingularity8@gmail.com>
2024-01-18 13:59:16 +0000

    Flask Babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,38 +1,23 @@
 #!/usr/bin/env python3
 """Flask App"""
 
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template
 from flask_babel import Babel
-
-app = Flask(__name__)
 
 class Config(object):
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
+app = Flask(__name__)
 app.config.from_object(Config)
-
-
-# def get_locale():
-#     """Return default language as en"""
-#     return request.accept_languages.best_match(['en', 'fr'])
-
-
-# def get_timezone():
-#     """Get Timezone else default to UTC"""
-#     user = getattr(g, 'user', None)
-#     if user is not None:
-#         return user.timezone
-#     return 'UTC'
-
 
 babel = Babel(app)
 
 
 @app.route('/')
-def home():
-    """Display homme page"""
+def index():
     return render_template('1-index.html')
 
 
